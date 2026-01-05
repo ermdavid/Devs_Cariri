ab42c36dd4f1b7325fc8be0a2ad7baf518f9c76c
diegodgkariri <diego.pereiragomes@gmail.com>
2026-01-05 10:21:07 -0300

    Adicionar classe ClienteCadastrar com método de cadastro

--- ClienteCadastrar.py
+++ ClienteCadastrar.py
@@ -1,1 +1,10 @@
- 
+class ClienteCadastrar:
+       def __init__(self, nome, email, telefone):
+            self.nome = nome
+            self.email = email
+            self.telefone = telefone
+            self.Endereco = []
+
+      def cadastrar(self):
+            # Lógica para cadastrar o cliente no sistema
+            print(f"Cliente {self.nome} cadastrado com sucesso!")
