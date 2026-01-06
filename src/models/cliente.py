54de6c4211bce6481368ffe0aa06e5e797cb77cc
diegodgkariri <diego.pereiragomes@gmail.com>
2026-01-05 21:44:19 -0300

    Atualizar classe Cliente com métodos de inicialização e comparação

--- src/models/cliente.py
+++ src/models/cliente.py
@@ -1,1 +1,12 @@
+class Cliente:
+    def __init__(self, id, nome, email, cpf):
+        self.id = id
+        self.nome = nome
+        self._email = email  # Privado para usar @property
+        self._cpf = cpf      # Privado para usar @property
+        self.enderecos = []  # Lista de objetos da classe Endereco
 
+    def __eq__(self, other):
+        if isinstance(other, Cliente):
+            return self.cpf == other.cpf or self.email == other.email
+        return False
