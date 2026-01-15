a5979f4bdde8fbb8770298e8ae30a9797708a41d
Ermeson David <77757310+ermdavid@users.noreply.github.com>
2026-01-15 19:49:18 -0300

    Implement menu for client and product management

--- src/cli/menu.py
+++ src/cli/menu.py
@@ -1,1 +1,33 @@
+from repository import clientes, produtos
+from models.cliente import Cliente
+from models.produto import Produto
 
+def menu():
+    print("\n=== LOJA DEVSCARIRI ===")
+    print("1 - Cadastrar Cliente")
+    print("2 - Cadastrar Produto")
+    print("3 - Listar Produtos")
+    print("4 - Gerar Relatório")
+    print("0 - Sair")
+
+def executar(opcao):
+    if opcao == "1":
+        id = int(input("ID: "))
+        nome = input("Nome: ")
+        email = input("Email: ")
+        cpf = input("CPF: ")
+        clientes.append(Cliente(id, nome, email, cpf))
+        print("Cliente cadastrado!")
+
+    elif opcao == "2":
+        sku = input("SKU: ")
+        nome = input("Nome: ")
+        categoria = input("Categoria: ")
+        preco = float(input("Preço: "))
+        estoque = int(input("Estoque: "))
+        produtos.append(Produto(sku, nome, categoria, preco, estoque))
+        print("Produto cadastrado!")
+
+    elif opcao == "3":
+        for p in produtos:
+            print(p)
