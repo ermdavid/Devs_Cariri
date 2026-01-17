2d516110590966c7d44a8a6026f29f150e22f18f
Fernando <fernando.oliveira@aluno.ufca.edu.br>
2026-01-17 15:29:07 -0300

    Update produto.py

--- src/models/produto.py
+++ src/models/produto.py
@@ -96,3 +96,34 @@ class Produto:
 
   def __ne__(self, other):
     return not self.__eq__(other)
+
+
+#Sub-classes
+#ProdutoDigital
+class ProdutoDigital(Produto):
+  def __init__(self, sku:str, nome:str, categoria:str, preco_unitario:float = 0.0, quantidade_estoque:int = 0, tamanho_mb:float = 0.0, situacao = "ativo", **kwargs):
+    super().__init__(sku, nome, categoria, preco_unitario, quantidade_estoque, situacao)
+    self.tamanho_mb = tamanho_mb
+
+#Propriedade dos atributos
+  @property
+  def tamanho_mb(self) -> float:
+    return self._tamanho_mb
+  @tamanho_mb.setter
+  def tamanho_mb(self, tamanho_mb:float):
+    if not isinstance(tamanho_mb, (int, float)):
+      raise ValueError("O tamanho deve ser um número")
+    self._tamanho_mb = float(tamanho_mb)
+
+#Métodos
+  def adicionar_estoque(self, quantidade_estoque):
+    super().adicionar_estoque(quantidade_estoque)
+
+  def remover_estoque(self, quantidade_estoque):
+    super().remover_estoque(quantidade_estoque)
+
+  def __str__(self):
+    exibir_detalhes = super().__str__()
+    return f'{exibir_detalhes}| Tipo: digital| Tamanho: {self.tamanho_mb}mb'
+
+#ProdutoFisico
