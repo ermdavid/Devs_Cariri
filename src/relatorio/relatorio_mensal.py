30ce6beee2fbe4827835d858fe4200fdb797d1ac
Ermeson David <77757310+ermdavid@users.noreply.github.com>
2026-01-18 22:44:08 -0300

    Update relatorio_mensal.py

--- src/relatorio/relatorio_mensal.py
+++ src/relatorio/relatorio_mensal.py
@@ -1,1 +1,21 @@
+from datetime import date
+from src.repositorios.repositorio_pedidos import RepositorioPedidos
+from src.utils.enums import StatusPedido
 
+def gerar_relatorio_mensal(ano: int, mes: int):
+    repo = RepositorioPedidos()
+    total = 0.0
+
+    with open(f"relatorio_mensal_{ano}_{mes:02d}.txt", "w", encoding="utf-8") as f:
+        for p in repo.listar_pedidos():
+            if p.data_criacao.year == ano and p.data_criacao.month == mes:
+                if p.status in (StatusPedido.PAGO, StatusPedido.ENTREGUE):
+                    total += p.calcular_total()
+                    f.write(
+                        f"Pedido {p.id} | "
+                        f"Total: R$ {p.calcular_total():.2f}\n"
+                    )
+
+        f.write(f"\nTOTAL DO MÊS: R$ {total:.2f}")
+
+    print("Relatório mensal gerado.")
