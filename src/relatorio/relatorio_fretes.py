aff7a824abdcc365734d7631d4e0434e52792dad
Ermeson David <77757310+ermdavid@users.noreply.github.com>
2026-01-18 22:42:52 -0300

    Update relatorio_fretes.py

--- src/relatorio/relatorio_fretes.py
+++ src/relatorio/relatorio_fretes.py
@@ -1,1 +1,16 @@
+from src.repositorios.repositorio_pedidos import RepositorioPedidos
 
+def gerar_relatorio_fretes():
+    repo = RepositorioPedidos()
+    pedidos = repo.listar_pedidos()
+
+    with open("relatorio_fretes.txt", "w", encoding="utf-8") as f:
+        for p in pedidos:
+            if p.frete:
+                f.write(
+                    f"Pedido {p.id} | "
+                    f"Cidade: {p.endereco_entrega.cidade} | "
+                    f"Frete: R$ {p.valor_frete:.2f}\n"
+                )
+
+    print("Relatório de fretes gerado.")
